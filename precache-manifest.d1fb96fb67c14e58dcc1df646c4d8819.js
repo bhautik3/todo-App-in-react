c361b5f244d1c7a706aa449ba3981a213aca8241
Bhautik <bhautik3456@gmail.com>
2020-05-30 18:02:10 +0530

    Updates

---
+++ precache-manifest.d1fb96fb67c14e58dcc1df646c4d8819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9a1595fab540de422e2f6af62281df",
+    "url": "/todo-App-in-react/index.html"
+  },
+  {
+    "revision": "d14156a27988287c6135",
+    "url": "/todo-App-in-react/static/css/main.e1291b6d.chunk.css"
+  },
+  {
+    "revision": "b36eec5c97a6ff35400b",
+    "url": "/todo-App-in-react/static/js/2.3b973db5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-App-in-react/static/js/2.3b973db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14156a27988287c6135",
+    "url": "/todo-App-in-react/static/js/main.9392914f.chunk.js"
+  },
+  {
+    "revision": "76619773928a119aaeb1",
+    "url": "/todo-App-in-react/static/js/runtime-main.2adc2f3e.js"
+  }
+]);
